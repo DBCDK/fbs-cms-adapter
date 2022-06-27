d53d8b959629910ff0d6dd878748183561b08869
pjo <pjo@dbc.dk>
2022-06-27 09:02:10 +0200

    downgrade fastify cors for 3.x compatibility

--- e2e/cypress/integration/adapter.spec.js
+++ e2e/cypress/integration/adapter.spec.js
@@ -31,7 +31,7 @@ describe("Testing the FBS CMS adapter", () => {
       }).then((res) => {
         expect(res.status).to.eq(400);
         expect(res.body).to.deep.include({
-          message: "headers must have required property 'authorization'",
+          message: "headers should have required property 'authorization'",
         });
       });
     });
@@ -841,6 +841,7 @@ function mockFetchFbsCmsAnonymousPathSucces() {
     },
   });
 }
+
 function mockFetchFbsCmsAnonymousPathPostSucces() {
   mockHTTP({
     request: {
